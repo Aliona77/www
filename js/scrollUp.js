0fd699cd2433fc03f84cc6b0d2b82f1c3cf30bda
Aliona77 <chabanalona153@gmail.com>
2023-07-20 16:22:54 +0200

    edited typo in education section

--- js/scrollUp.js
+++ js/scrollUp.js
@@ -1,6 +1,4 @@
 
-
-
 document.addEventListener("DOMContentLoaded", function () {
   const footer = document.getElementById("footer");
   const scrollToTopBtn = document.getElementById("scrollToTopBtn");
